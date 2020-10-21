2a0b22931f97f6ee8ea98f6f771e9c9266e211d5
Daniel Wright <dan@Daniels-MacBook-Air.local>
2020-10-21 13:36:24 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MyInfo from './components/MyInfo.js';
+
+function App() {
+  return (
+    <div className="App">
+      <MyInfo />
+      <p>
+        Edit src/App.js hy.
+      </p>
+    </div>
+  );
+}
+
+export default App;
