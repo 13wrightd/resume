72c61542e6576d8d88523bd30f7daa1edbae98d8
Daniel Wright <dan@Daniels-MacBook-Air.local>
2020-10-23 11:41:41 -0400

    form practice

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,25 @@
 import React from "react"
 
-function Product(props){
-    return (
-        <div>
-            <h1>{props.name}</h1>
-            <br></br>
-            <h2>price: ${props.price}</h2>
-            <br></br>
-            <h3>description: {props.description}</h3>
-            <br></br>
-            <br></br>
-        </div>
-    )
+class Product extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            name:"Daniel Wright"
+        }
+    }
+    render(){
+        return (
+            <div>
+                <h1>{this.state.name}</h1>
+                <br></br>
+                <h2>price: ${this.props.price}</h2>
+               
+                <h3>desciption: {this.props.description}</h3>
+                <br></br>
+
+            </div>
+        )
+    }
 }
 
 export default Product
